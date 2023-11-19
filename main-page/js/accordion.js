5b967e8a1ded609e732a902b20a226b86f301362
Na1x-dev <na1x.1024@gmail.com>
2023-11-19 13:04:38 +0300

    feat: now accordion is working good

--- main-page/js/accordion.js
+++ main-page/js/accordion.js
@@ -17,16 +17,16 @@ function showAccordionPartContent(accordPart) {
     let accordPartContent = accordPart.querySelector('.about-accordion-content-container');
     let accordPartContentText = accordPart.querySelector('.about-accordion-text-content');
     let accordPartContentImg = accordPart.querySelector('.about-accordion-image-content');
-    accordPartContent.style.maxHeight = '1000px';    
-    accordPartContentImg.style.maxHeight = '1000px';
+    accordPartContent.style.height = '150px';    
+    // accordPartContentImg.style.height = '25%';
 }
 
 function hideAccordionPartContent(accordPart) {
     let accordPartContent = accordPart.querySelector('.about-accordion-content-container');
     let accordPartContentText = accordPart.querySelector('.about-accordion-text-content');
     let accordPartContentImg = accordPart.querySelector('.about-accordion-image-content');
-    accordPartContent.style.maxHeight = '0';
-    accordPartContentImg.style.maxHeight = '0';
+    accordPartContent.style.height = '0px';
+    // accordPartContentImg.style.height = '0px';
 }
 
 function changeAccordionPartContent(accordPart) {
@@ -44,13 +44,13 @@ function changeAccordionPartContent(accordPart) {
 function showPlusIcon(accordPart) {
     let accordPlusIcon = accordPart.querySelector('.about-accordion-heading-icon');
     accordPlusIcon.style.clipPath = 'polygon(0 48%, 48% 48%, 48% 0, 52% 0, 52% 48%, 100% 48%, 100% 52%, 52% 52%, 52% 100%, 48% 100%, 48% 52%, 0 52%)';
-    accordPlusIcon.style.transform = 'rotate(0deg)';
+    // accordPlusIcon.style.transform = 'rotate(0deg)';
 }
 
 function showMinusIcon(accordPart) {
     let accordPlusIcon = accordPart.querySelector('.about-accordion-heading-icon');
     accordPlusIcon.style.clipPath = 'polygon(48% 0, 52% 0, 52% 100%, 48% 100%)';
-    accordPlusIcon.style.transform = 'rotate(270deg)';
+    // accordPlusIcon.style.transform = 'rotate(90deg)';
 }
 
 //accordion end
